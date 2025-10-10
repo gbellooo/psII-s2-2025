328b891abe7a7f3771bf43f769e74b83f2420eb3
Gabriel Bello <125406298+gbellooo@users.noreply.github.com>
2025-10-10 23:51:33 +0000

    criação da estrutura

---
+++ prj1a/src/main/java/com/example/prj1a/Prj1aApplication.java
@@ -0,0 +1,13 @@
+package com.example.prj1a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj1aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj1aApplication.class, args);
+	}
+
+}
---
+++ prj1a/src/test/java/com/example/prj1a/Prj1aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj1a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj1aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
